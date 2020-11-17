c4b9b4745f1d14a7aeac96de2118571ef7e5743c
Qt君 <2088201923@qq.com>
2020-11-17 15:30:31 +0800

    更新数字键盘demo

--- main.cpp
+++ main.cpp
@@ -13,18 +13,7 @@ LICENSE: MIT
 
 using namespace AeaQt;
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-
-    QWidget window;
-    window.setWindowTitle("QtKeyboard by Qt君");
-    window.resize(850, 370);
-    NumberKeyboard keyboard;
-    keyboard.show();
-    QLineEdit textInput(&keyboard);
-    textInput.setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
-    textInput.setStyleSheet("                              \
+static const QString qss = "                               \
                             QLineEdit {                    \
                                 border-style: none;        \
                                 padding: 3px;              \
@@ -32,7 +21,22 @@ int main(int argc, char *argv[])
                                 border: 1px solid #dce5ec; \
                                 font-size: 30px;           \
                             }                              \
-                            ");
+                            ";
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("QtKeyboard by Qt君");
+    window.resize(850, 370);
+
+    Keyboard keyboard;
+    keyboard.show();
+
+    QLineEdit textInput(&keyboard);
+    textInput.setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    textInput.setStyleSheet(qss);
 
     QVBoxLayout *v = new QVBoxLayout;
     v->addWidget(&textInput, 1);
@@ -40,6 +44,25 @@ int main(int argc, char *argv[])
 
     window.setLayout(v);
     window.show();
+
+    ///////////////////////////////
+    QWidget window2;
+    window2.setWindowTitle("NumberKeyboard by Qt君");
+    window2.resize(550, 370);
+
+    NumberKeyboard keyboard2;
+    keyboard2.show();
+
+    QLineEdit textInput2(&keyboard2);
+    textInput2.setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+    textInput2.setStyleSheet(qss);
+
+    QVBoxLayout *v2 = new QVBoxLayout;
+    v2->addWidget(&textInput2, 1);
+    v2->addWidget(&keyboard2, 5);
+
+    window2.setLayout(v2);
+    window2.show();
 
     return a.exec();
 }
